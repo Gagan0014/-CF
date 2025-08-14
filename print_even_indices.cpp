9e6b0a4c1162929ae7d503568688efe5f2e79bff
Gagan0014 <gaganpathak2238@gmail.com>
2025-08-14 10:50:19 +0530

    print even indices

---
+++ print_even_indices.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+void sol(vector<int>arr,int n){
+    if(n==0){
+        cout<<arr[n];
+        return ;
+    }
+    if(n%2==0){
+        cout<<arr[n]<<" ";  
+        sol(arr,n-2);
+    }else{
+        sol(arr,n-1);
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    sol(arr,n-1);
+    return 0;
+}
